07354091b5fb21065a25c022394bc6489a26931e
Lauri Ajanki <laareni@yahoo.co.uk>
2017-09-09 18:34:34 +0300

    Added tests for Randomizer and QuoteRandomizer

--- test/test_quotes.py
+++ test/test_quotes.py
@@ -10,163 +10,178 @@ import sqlite3 as lite
 sys.path.append("../")  # append the main script folder to sys.path to be able to import quotes from it
 import quotes
 
-# Tests for the switch and string normalization functions in quote.py
-
 
 class RandomizeTestCase(unittest.TestCase):
-	"""Does the randomization for quotes, facts and lyrics work properly?"""
+    """Test cases for the base Randomizer class."""
 
-	@classmethod
-	def setUpClass(self):
-		"""Create a randomizer."""
-		self.randomizer = quotes.Randomizer()
+    @classmethod
+    def setUpClass(self):
+        """Create a randomizer."""
+        self.randomizer = quotes.Randomizer("../")
+
+    @classmethod
+    def tearDownClass(self):
+        del self.randomizer
+
+    def testIOErrorOnInvalidPath(self):
+        """Does trying to create a Generator object with non-existing path raise an error?"""
+        path = "noSuchPath"
+        self.assertRaises(IOError, quotes.Randomizer, path)
+
+    def testChangeDegreeForShortString(self):
+        """The computed change degree for a short string should be 1"""
+        tokens = "Short message".split()
+        change_degree = self.randomizer.get_change_degree(tokens)
+        self.assertEqual(change_degree, 1)
+
+    def testChangeDegreeInRange(self):
+        """Is the change degree in [1,3]?"""
+        tokens = "Oh, my this is a wonderful treat. I shall treasure this for at least a few seconds, possibly longer!".split()
+        change_degree = self.randomizer.get_change_degree(tokens)
+        self.assertGreaterEqual(change_degree, 1)
+        self.assertLessEqual(change_degree, 3)
+
+    def testChooseReplacingWordsKeepsNumberOfWordsIntact(self):
+        """Does choose_replacing_words generate equal number of replacing words as was passed in?"""
+        tokens =  [(0, "carbon", "NN"), (1, "feeling", "NN")]
+        replacing_words = self.randomizer.choose_replacing_words(tokens)
+        self.assertEqual(len(replacing_words), 2)
+
+    def testChooseReplacingWordsKeepsCapitalizationAndIndicesIntact(self):
+        """Does choose_replacing_words keep capitalization and word indices intact?"""
+        tokens =  [(0, "Carbon", "NN"), (1, "FEELING", "NN")]
+        replacing_words = self.randomizer.choose_replacing_words(tokens)
+
+        # first word
+        self.assertEqual(replacing_words[0][0], 0)
+        word = replacing_words[0][1]
+        self.assertEqual(word, word.capitalize())
+
+        # second word
+        self.assertEqual(replacing_words[1][0], 1)
+        word = replacing_words[1][1]
+        self.assertEqual(word, word.upper())
+
+    def testChooseReplacingWordsKeepsPOSTagsIntact(self):
+        """Does choose_replacing_words replace with the same POS tag?"""
+        tokens =  [(0, "green", "JJ"), (1, "carbon", "NN")]
+        replacing_words = self.randomizer.choose_replacing_words(tokens)
+
+        # use nltk to find out the new words' tags
+        new_words = [token[1] for token in replacing_words]
+        tagged = nltk.pos_tag(new_words)
+
+        tag1 = tagged[0][1]
+        self.assertEqual(tag1, "JJ")
+        tag2 = tagged[1][1]
+        self.assertEqual(tag2, "NN")
+
+    def testWordSwitching(self):
+        """Do the correct words get switched in switch_word_tokens?"""
+        original = "Big Bertha on the long and winding street".split()
+        new_tokens = [(0, "Small"), (2, "against"), (6, "relaxing"), (7, "pavement")]
+        expected = "Small Bertha against the long and relaxing pavement".split()
+
+        switched = self.randomizer.switch_word_tokens(original, new_tokens)
+        self.assertEqual(switched, expected)
+
+    def testRandomizedStringIsEqualLength(self):
+        """Is the string generated equal in length to the original?"""
+        s = "Sometimes winter never comes, other times James's birthday is in July, but never after the fifth."
+        new = self.randomizer.randomize_string(s)
+        self.assertEqual(len(s.split()), len(new.split()))
+
+    def testRandomizingRaisesErrorOnInvalidInput(self):
+        """Does randomize_string raise ValueError if there are no valid words in the input?"""
+        s = ", !!! :) http"
+        self.assertRaises(ValueError, self.randomizer.randomize_string, s)
+
+    def testNormalization(self):
+        """Does token normalization remove extra whitespace around a single quote?"""
+        tokens = "Bender 's game".split()
+        s = " ".join(quotes.Randomizer.normalize_tokens(tokens))
+
+        self.assertEqual(s, "Bender's game")
+
+    def testStringCleanup(self):
+        """Does cleanup remove whitespace around punctuation?"""
+        s1 = "Win , for me !"
+        s1_expected = "Win, for me!"
+
+        s2 = "some guy @ babylon # sweet"
+        s2_expected = "some guy @babylon #sweet"
+
+        s3 = "http: //www.somesite.com"
+        s3_expected = "http://www.somesite.com"
+
+        s4 = "this be a `` quote''"
+        s4_expected = "this be a \"quote\""
+
+        s5 = "The Vindicators : Tomorrow's game"
+        s5_expected = "The Vindicators: Tomorrow's game"
+
+        self.assertEqual(quotes.Randomizer.cleanup(s1), s1_expected)
+        self.assertEqual(quotes.Randomizer.cleanup(s2), s2_expected)
+        self.assertEqual(quotes.Randomizer.cleanup(s3), s3_expected)
+        self.assertEqual(quotes.Randomizer.cleanup(s4), s4_expected)
+        self.assertEqual(quotes.Randomizer.cleanup(s5), s5_expected)
 
 
-	@classmethod
-	def tearDownClass(self):
-		del self.randomizer
+class QuoteRandomizerTestCase(unittest.TestCase):
+    """QuoteRandomizer test cases."""
+
+    @classmethod
+    def setUpClass(self):
+        """Create a randomizer."""
+        self.randomizer = quotes.QuoteRandomizer("../")
+
+    @classmethod
+    def tearDownClass(self):
+        del self.randomizer
+
+    def testRandomizedQuoteNotTheSame(self):
+        """Is the generated quote different from the original quote?"""
+        quote, author = self.randomizer.get_quote()
+        randomized = self.randomizer.randomize_string(quote)
+
+        self.assertNotEqual(quote, randomized)
 
 
-	def test_quote_randomizer(self):
-		"""Is the result of generating a quote a tuple?
-		Mostly to check the functions don't throw errors.
-		"""
-		quote = self.randomizer.get_quote()
-		rand = self.randomizer.randomize(quote)
-		self.assertIsInstance(rand, tuple)
-
-
-	def test_fact_randomizer(self):
-		"""Is the result of generating a fact a tuple?"""
-		fact = self.randomizer.get_fact()
-		rand = self.randomizer.randomize(fact)
-		self.assertIsInstance(rand, tuple)
-
-
-
-class SwitchTestCase(unittest.TestCase):
-	"""Tests regarding switch() behaviour."""
-
-	@classmethod
-	def setUpClass(self):
-		"""Read the quotes from the test database."""
-		con = lite.connect("./test_quotes.db")
-		cur = con.cursor()
-		randomizer = quotes.Randomizer() # only needed in setup
-
-		with con:
-			cur.execute("SELECT * FROM quotes")
-			self.quotes = cur.fetchall()
-
-		# drop authors and randomize the quotes
-		self.orig_quotes = [q[0] for q in self.quotes]
-		self.processed = [randomizer.switch(q) for q in self.orig_quotes]  # items are dicts with "randomized", "old" and "new" keys
-
-
-	def test_switch_length(self):
-		"""Are the lengths of the original quote and the randomized quote still the same?"""
-		#orig_lengths = [len(q.split()) for q in self.orig_quotes]
-		#switch_lengths = [len(q["randomized"].split()) for q in self.processed]
-
-		# compare each length individually to keep track of which quote fails (if any)
-		for i, quote in enumerate(self.orig_quotes):
-			orig_quote = self.orig_quotes[i]
-			switch_quote = self.processed[i]["randomized"]
-
-			self.assertEqual(len(switch_quote.split()), len(orig_quote.split()), "The lengths of '{}' and\n'{}' are not equal.".format(orig_quote, switch_quote))
-
-
-	def test_switch_amount(self):
-		"""Did switch() make the correctnumber of switches?"""
-		for i, quote in enumerate(self.processed):
-			nswitch = len(quote["old"])  # number of words that should have been changed
-			orig_split = self.orig_quotes[i].split()
-			switch_split = quote["randomized"].split()
-
-			# loop over the words of the switch quote and count number of non-equal matches to the original quote
-			diffs = 0
-			for j, word in enumerate(switch_split):
-				if word != orig_split[j]:
-					diffs += 1
-
-			self.assertEqual(diffs, nswitch, "There should be {} changes between '{}' and '{}'".format(nswitch, self.orig_quotes[i], quote["randomized"]))
-
-
-
-class NormalizeTestCase(unittest.TestCase):
-	"""String normalization test."""
-
-	@classmethod
-	def setUpClass(self):
-		"""Read the quotes from the test database."""
-		con = lite.connect("./test_quotes.db")
-		cur = con.cursor()
-		randomizer = quotes.Randomizer()
-
-		with con:
-			cur.execute("SELECT * FROM quotes")
-			self.quotes = cur.fetchall()
-
-		# drop author and randomize the quotes
-		self.orig_quotes = [q[0] for q in self.quotes]
-		self.processed = [randomizer.switch(q) for q in self.orig_quotes]  # items are dicts with "randomized", "old" and "new" keys
-
-
-	def test_normalization(self):
-		"""Does a word start with an apostrophe (')?"""
-		for quote in self.processed:
-			split = quote["randomized"].split()
-			has_apostrophe_starts = any([x.startswith("'") for x in split])
-			self.assertFalse(has_apostrophe_starts, "Found a word starting with a ' in {}".format(quote["randomized"]))
-
-
+@unittest.skip("Old behaviour")
 class SongProcessTestCase(unittest.TestCase):
-	"""Does repeated calls to get_next_lyric() process a song properly?"""
+    """Does repeated calls to get_next_lyric() process a song properly?"""
 
-	@classmethod
-	def setUpClass(self):
-		self.randomizer = quotes.Randomizer()
+    @classmethod
+    def setUpClass(self):
+        self.randomizer = quotes.Randomizer()
 
-	@classmethod
-	def tearDownClass(self):
-		del self.randomizer
+    @classmethod
+    def tearDownClass(self):
+        del self.randomizer
 
-	def test_song_change(self):
-		"""Is the return value of get_next_lyric() a tuple of two non-empty values
-		after a song change?
-		"""
-		self.randomizer.set_song("Californication")
-		lyric = self.randomizer.get_next_lyric()
-		self.assertIsInstance(lyric, tuple)
-		self.assertIsNotNone(lyric[0])
-		self.assertIsNotNone(lyric[1])
-		
-
-	def test_full_song_process(self):
-		"""Is the return value of get_next_lyric() a tuple on the first n calls
-		and None on any subsequent calls?
-		"""
-		self.randomizer.set_song("What a Wonderful World") # 8 lines in the database
-
-		# First 8 calls should return a tuple
-		for i in range(8):
-			lyric = self.randomizer.get_next_lyric()
-			self.assertIsInstance(lyric, tuple)
-
-		# subsequent calls should be None
-		for i in range(2):
-			lyric = self.randomizer.get_next_lyric()
-			self.assertIsNone(lyric)
+    def test_song_change(self):
+        """Is the return value of get_next_lyric() a tuple of two non-empty values
+        after a song change?
+        """
+        self.randomizer.set_song("Californication")
+        lyric = self.randomizer.get_next_lyric()
+        self.assertIsInstance(lyric, tuple)
+        self.assertIsNotNone(lyric[0])
+        self.assertIsNotNone(lyric[1])
 
 
+    def test_full_song_process(self):
+        """Is the return value of get_next_lyric() a tuple on the first n calls
+        and None on any subsequent calls?
+        """
+        self.randomizer.set_song("What a Wonderful World") # 8 lines in the database
 
+        # First 8 calls should return a tuple
+        for i in range(8):
+            lyric = self.randomizer.get_next_lyric()
+            self.assertIsInstance(lyric, tuple)
 
-
-
-
-if __name__ == '__main__':
-	# Create a test suite and run the tests.
-	# Note that this only matters when this script is run directly,
-	# ie. using python -m unittest -v test_quotes will run all tests!
-	suite = unittest.TestLoader().loadTestsFromTestCase(SwitchTestCase)
-	unittest.TextTestRunner(verbosity=2).run(suite)
+        # subsequent calls should be None
+        for i in range(2):
+            lyric = self.randomizer.get_next_lyric()
+            self.assertIsNone(lyric)
