520fc2fa3f3ef9ff72f0d03114e84b5aacf33596
Lauri Ajanki <laareni@yahoo.co.uk>
2017-09-11 21:11:14 +0300

    Initial formatting

--- main.py
+++ main.py
@@ -1,79 +1,78 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-	def main(self, args):
-		"""Process command line arguments."""
+import quotes
+import dbaccess
+import nltk
+import argparse
 
-		# If no optional parameter was provided, also recreate the dictionary.
-		if args.update_database:
-			quick = False
-			mode = args.update_database # check for optional parameter, if none default to 'full'
+def main(args):
+    """Process command line arguments."""
 
-			if mode not in ["full", "quick"]:
-				print "Error: invalid mode, please use either 'full' (default) or 'quick'."
-				sys.exit()
+    # If no optional parameter was provided, also recreate the dictionary.
+    if args.build_database:
+        pass
 
-			if mode == "quick":
-				quick = True
-			if mode == "full":
-				ans = raw_input("The database will be parsed for new words in the dictionary.\
-				This may take some time, continue? (y/N)\n")
-				if ans != "y":
-					print "Exiting."
-					sys.exit()
+    elif args.quote:
+        randomize_quote()
 
-			dbaccess.update_db(quick)
+    elif args.fact:
+        randomize_fact()
 
-		elif args.quote:
-			quote = self.get_quote()
-			rand = self.randomize(quote)
-			print rand[0] + "\n--" + rand[1]
+    elif args.song:
+        pass
 
-		elif args.fact:
-			fact = self.get_fact()
-			rand = self.randomize(fact)
-			print rand[0] + "\n--" + rand[1]
+    elif args.set_song:
+        pass
 
-		elif args.song:
-			lyric = self.randomize_lyric()
-			if lyric[0]:
-				print lyric[0] + "\n" + lyric[1]
-			else:
-				print lyric[1]
+    elif args.size:
+        dbaccess.database_size()
 
-		elif args.set_song:
-			if args.set_song == "list":
-				for name in self.get_songs():
-					print name
-
-			else:
-				self.set_song(args.set_song)
-
-		elif args.size:
-			dbaccess.database_size()
-
-		elif args.tags:
-			nltk.help.upenn_tagset()
+    elif args.tags:
+        nltk.help.upenn_tagset()
 
 
+#============================================================================
+# Define a function for each command line arg #
+#==============================================
+def build_quote_database():
+    """Drop all existing data from quotes.db refill it by executing quotes.sql.
+    TODO: switch for optional dictionary parsing?
+    """
+    dbaccess.create_quote_database()
+
+def build_song_database():
+    """Drop all existing data from songs.db refill it by executing songss.sql.
+    TODO: switch for optional dictionary parsing?
+    TODO: merge with above?
+    """
+    dbaccess.create_song_database()
+
+def randomize_quote():
+    """Create a new randomized quote or a fact."""
+    randomizer = quotes.QuoteRandomizer()
+    quote, author = randomizer.generate()
+    print quote + "\n--" + author
+
+def randomize_fact():
+    """Create a new randomized fact."""
+    randomizer = quotes.QuoteRandomizer()
+    fact, _ = randomizer.get_fact()
+    randomized = randomizer.randomize_string(fact)
+    print fact
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="A quote randomizer.")
-	parser.add_argument("--quote", help="Generate a randomized quote.", action="store_true")
-	parser.add_argument("--fact", help="Generate a randomized fact.", action="store_true")
-	parser.add_argument("--song", help="Generate the next song lyric for the current song. Use --set-song to initialize a new song.", action="store_true")
-	parser.add_argument("--set-song", metavar="song", help="Sets the given song to be the next one read by --song. Use [list] to see valid choices.")
-	parser.add_argument("--update-database", nargs="?", metavar="mode", const="full", help="""Fills the database by executing quotes.sql.
-		If no mode is set, the quotes and lyrics tables are parsed for new words to add to the dictionary.
-		If mode is set to 'quick' the dictionary is not modified.""")
-	parser.add_argument("--size", help="Shows the size of the databse.", action="store_true")
-	parser.add_argument("--tags", help="Shows info on all tags used to categorize words into classes.", action="store_true")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="A quote randomizer.")
+    parser.add_argument("--quote", help="Generate a randomized quote.", action="store_true")
+    parser.add_argument("--fact", help="Generate a randomized fact.", action="store_true")
+    parser.add_argument("--song", help="Generate the next song lyric for the current song. Use --set-song to initialize a new song.", action="store_true")
+    parser.add_argument("--set-song", metavar="song", help="Sets the given song to be the next one read by --song. Use [list] to see valid choices.")
+    parser.add_argument("--build-database", nargs="?", metavar="mode", const="full", help="""Fills the database by executing quotes.sql.
+        If no mode is set, the quotes and lyrics tables are parsed for new words to add to the dictionary.
+        If mode is set to 'quick' the dictionary is not modified.""")
+    parser.add_argument("--size", help="Shows the size of the databse.", action="store_true")
+    parser.add_argument("--tags", help="Shows info on all tags used to categorize words into classes.", action="store_true")
+    args = parser.parse_args()
 
-	app = Randomizer()
-	try:
-		app.main(args)
-	# switch() had nothing to switch (no valid tags)
-	except ValueError as e:
-		print e
+    main(args)
